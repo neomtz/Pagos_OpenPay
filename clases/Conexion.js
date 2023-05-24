2fc75b13c86b6772edbe068705d3138f62a288c2
Neo Martinez <neomtz2951@gmail.com>
2023-05-24 16:15:58 -0600

    2023-05-23

--- clases/Conexion.js
+++ clases/Conexion.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
-mongoose.connect('mongodb://m0ng0db:m0ng0dbpr3pr0d@52.117.21.213:27017/jarbos_patrimoniohoydb', {
+mongoose.connect('mongodb://m0ng0db:m0ng0dbpr3pr0d@ip/neo_patrimoniohoydb', {
     useNewUrlParser: true,
     useUnifiedTopology: true,
     authSource:"admin",
